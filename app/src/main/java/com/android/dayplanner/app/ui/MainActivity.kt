5940ed5d0abcd7ea4345d653bda4f275302744ef
Junaid Ahmad <junaid.ahmed@callsign.com>
2021-09-04 18:12:38 +0400

    Integration of Android 12 Splash API

--- app/src/main/java/com/android/dayplanner/app/ui/MainActivity.kt
+++ app/src/main/java/com/android/dayplanner/app/ui/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.android.dayplanner.app.ui
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnticipateInterpolator
 import androidx.annotation.VisibleForTesting
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.splashscreen.SplashScreenViewProvider
 import androidx.room.Room
 import androidx.test.espresso.IdlingResource
 import com.android.dayplanner.app.R
@@ -16,7 +23,40 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val splashScreen = installSplashScreen()
         setContentView(R.layout.activity_main)
+
+        setSplashExitAnimation(splashScreen)
+    }
+
+    private fun setSplashExitAnimation(splashScreen: SplashScreen) {
+       // SimpleIdlingResource.setIdleState(false)
+        splashScreen.setOnExitAnimationListener { splashScreenView ->
+            configureObjectAnimator(splashScreenView) { slideUpAnimation ->
+                with(slideUpAnimation) {
+                    interpolator = AnticipateInterpolator()
+                    duration = 3000L
+                    doOnEnd {
+                        splashScreenView.remove()
+                      //  SimpleIdlingResource.setIdleState(true)
+                    }
+                    start()
+                }
+            }
+        }
+    }
+
+    private fun configureObjectAnimator(
+        splashScreenView: SplashScreenViewProvider,
+        onComplete: (ObjectAnimator) -> Unit
+    ) {
+        val objectAnimator = ObjectAnimator.ofFloat(
+            splashScreenView.view,
+            View.TRANSLATION_Y,
+            0f,
+            -splashScreenView.view.height.toFloat()
+        )
+        onComplete.invoke(objectAnimator)
     }
 
     fun getDatabaseInstance(): TaskDatabase {
