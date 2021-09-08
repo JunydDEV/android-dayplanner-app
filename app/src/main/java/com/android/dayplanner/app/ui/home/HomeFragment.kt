9400114689eef43017f48cd5345dbbc8010f63cc
Junaid Ahmad <junaid.ahmed@callsign.com>
2021-09-09 00:51:45 +0400

    Converted home fragment to views binding

--- app/src/main/java/com/android/dayplanner/app/ui/home/HomeFragment.kt
+++ app/src/main/java/com/android/dayplanner/app/ui/home/HomeFragment.kt
@@ -1,27 +1,27 @@
 package com.android.dayplanner.app.ui.home
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Bundle
 import android.view.*
-import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.widget.Toolbar
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.android.dayplanner.app.R
 import com.android.dayplanner.app.addAll
 import com.android.dayplanner.app.data.Task
+import com.android.dayplanner.app.databinding.HomeFragmentBinding
 import com.android.dayplanner.app.ui.MainActivity
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.pedrogomez.renderers.RVRendererAdapter
 import com.pedrogomez.renderers.RendererBuilder
 
 
-class HomeFragment : Fragment(R.layout.home_fragment) {
+class HomeFragment : Fragment() {
 
+    private lateinit var binding: HomeFragmentBinding
     private lateinit var mainActivity: MainActivity
 
     private val viewModel by viewModels<HomeViewModel> {
@@ -38,9 +38,6 @@ class HomeFragment : Fragment(R.layout.home_fragment) {
 
     private val tasksAdapter by lazy { RVRendererAdapter(rendererBuilder, tasksList) }
 
-    private lateinit var placeholder: ViewGroup
-    private lateinit var seeAllTasks: TextView
-
     override fun onAttach(context: Context) {
         super.onAttach(context)
         mainActivity = context as MainActivity
@@ -51,39 +48,49 @@ class HomeFragment : Fragment(R.layout.home_fragment) {
         setHasOptionsMenu(true)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    @SuppressLint("NotifyDataSetChanged")
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
 
-        placeholder = view.findViewById(R.id.placeholder)
+        binding = DataBindingUtil.inflate(inflater, R.layout.home_fragment, container, false)
 
+        //viewModel invoking
         viewModel.loadPendingTasks()
-
-        viewModel.tasksListLiveData.observe(viewLifecycleOwner, { list ->
+        viewModel.tasksListLiveData.observe(viewLifecycleOwner) { list ->
             tasksList.addAll(trashExistingItems = true, list)
             tasksAdapter.notifyDataSetChanged()
-            placeholder.visibility = if (list.isNotEmpty()) {
+            binding.placeholder.visibility = if (list.isNotEmpty()) {
                 View.GONE
             } else {
                 View.VISIBLE
             }
-        })
+        }
 
-        view.findViewById<RecyclerView>(R.id.recyclerView).run {
+        //binding invoking
+        binding.recyclerView.apply {
             layoutManager = LinearLayoutManager(requireContext())
             adapter = tasksAdapter
         }
-
-        view.findViewById<Toolbar>(R.id.toolbar).setOnMenuItemClickListener {
+        binding.toolbar.setOnMenuItemClickListener {
             onOptionsItemSelected(it)
         }
-
-        view.findViewById<FloatingActionButton>(R.id.floating_action_button).setOnClickListener {
+        binding.floatingActionButton.setOnClickListener {
             findNavController().navigate(R.id.action_homeFragment_to_taskDetailsFragment)
         }
+
+        return binding.root
     }
 
+
     private fun gotoTaskDetails(task: Task) {
-        findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToTaskDetailsFragment(task))
+        findNavController().navigate(
+            HomeFragmentDirections.actionHomeFragmentToTaskDetailsFragment(
+                task
+            )
+        )
     }
 
     private fun registerTaskViewsClickListeners(view: View, task: Task) {
@@ -105,13 +112,13 @@ class HomeFragment : Fragment(R.layout.home_fragment) {
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.history,menu)
+        inflater.inflate(R.menu.history, menu)
         super.onCreateOptionsMenu(menu, inflater)
     }
 
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if(item.itemId == R.id.showHistory){
+        if (item.itemId == R.id.showHistory) {
             findNavController().navigate(R.id.action_homeFragment_to_tasksFragment)
         }
         return true
