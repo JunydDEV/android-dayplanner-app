7e1e6fa57db51a36946e6ab4ac9283eab5f2457f
Junaid Ahmad <junaid.ahmed@callsign.com>
2021-09-09 01:04:07 +0400

    Converted task details fragment to views binding

--- app/src/main/java/com/android/dayplanner/app/ui/detail/TaskDetailsFragment.kt
+++ app/src/main/java/com/android/dayplanner/app/ui/detail/TaskDetailsFragment.kt
@@ -2,22 +2,29 @@ package com.android.dayplanner.app.ui.detail
 
 import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.findNavController
 import com.android.dayplanner.app.R
 import com.android.dayplanner.app.data.Task
+import com.android.dayplanner.app.databinding.TaskDetailsFragmentBinding
 import com.android.dayplanner.app.ui.MainActivity
 import java.util.*
 
 class TaskDetailsFragment : Fragment(R.layout.task_details_fragment) {
 
     private lateinit var mainActivity: MainActivity
+    private lateinit var binding: TaskDetailsFragmentBinding
+
     private val viewModel by viewModels<TaskDetailsViewModel> {
         TaskViewModelFactory(mainActivity.getDatabaseInstance())
     }
@@ -27,32 +34,36 @@ class TaskDetailsFragment : Fragment(R.layout.task_details_fragment) {
         mainActivity = context as MainActivity
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.task_details_fragment, container, false)
 
-        arguments?.getParcelable<Task>("keyTask").run {
+        arguments?.getParcelable<Task>("keyTask").apply {
             this?.let {
-                view.findViewById<EditText>(R.id.editText_title).run {
+                binding.editTextTitle.apply {
                     setText(title)
                 }
-                view.findViewById<EditText>(R.id.editText_description).run {
+                binding.editTextDescription.apply {
                     setText(description)
                 }
-                view.findViewById<EditText>(R.id.editText_taskDate).run {
+                binding.editTextTaskDate.apply {
                     setText(createDate)
                 }
-                view.findViewById<Button>(R.id.button_saveTask).run {
+                binding.buttonSaveTask.apply {
                     text = context.getString(R.string.edit)
                 }
 
-                view.findViewById<Toolbar>(R.id.toolbar).run {
-                    title = "Edit Task"
+                binding.toolbar.apply {
+                    title = context.getString(R.string.button_edit_task)
                 }
-
             }
         }
 
-        view.findViewById<Button>(R.id.button_saveTask).run {
+        binding.buttonSaveTask.apply {
             setOnClickListener {
                 if (arguments != null) {
                     viewModel.updateTask(getUpdateTask()!!) {
@@ -68,58 +79,58 @@ class TaskDetailsFragment : Fragment(R.layout.task_details_fragment) {
             }
         }
 
-        view.findViewById<Button>(R.id.button_createLongTempTask).run {
+        binding.buttonCreateLongTempTask.apply {
             setOnClickListener {
-                view.findViewById<EditText>(R.id.editText_title).run {
+                binding.editTextTitle.apply {
                     setText(context.getString(R.string.dummy_brief_task_title))
                 }
-                view.findViewById<EditText>(R.id.editText_description).run {
+                binding.editTextDescription.apply {
                     setText(context.getString(R.string.dummy_brief_task_description))
                 }
-                view.findViewById<EditText>(R.id.editText_taskDate).run {
+                binding.editTextTaskDate.apply {
                     setText(Calendar.getInstance().time.toString())
                 }
             }
         }
 
-        view.findViewById<Button>(R.id.button_createShortTempTask).run {
+        binding.buttonCreateShortTempTask.apply {
             setOnClickListener {
-                view.findViewById<EditText>(R.id.editText_title).run {
+                binding.editTextTitle.apply {
                     setText(context.getString(R.string.dummy_short_task_title))
                 }
-                view.findViewById<EditText>(R.id.editText_description).run {
+                binding.editTextDescription.apply {
                     setText(context.getString(R.string.dummy_short_task_description))
                 }
-                view.findViewById<EditText>(R.id.editText_taskDate).run {
+                binding.editTextTaskDate.apply {
                     setText(Calendar.getInstance().time.toString())
                 }
             }
         }
 
+        return binding.root
     }
 
     private fun createTask(): Task {
         return Task(
-            title = getValueFromField(R.id.editText_title),
-            description = getValueFromField(R.id.editText_description),
-            createDate = getValueFromField(R.id.editText_taskDate)
+            title = getValueFromField(binding.editTextTitle),
+            description = getValueFromField(binding.editTextDescription),
+            createDate = getValueFromField(binding.editTextTaskDate)
         )
     }
 
     private fun getUpdateTask(): Task? {
         arguments?.getParcelable<Task>("keyTask")?.run {
-            title = getValueFromField(R.id.editText_title)
-            description = getValueFromField(R.id.editText_description)
-            createDate = getValueFromField(R.id.editText_taskDate)
+            title = getValueFromField(binding.editTextTitle)
+            description = getValueFromField(binding.editTextDescription)
+            createDate = getValueFromField(binding.editTextTaskDate)
             return this
         }
         return null
     }
 
-    private fun getValueFromField(id: Int): String {
-        view?.findViewById<EditText>(id)?.run {
+    private fun getValueFromField(view: TextView): String {
+        view.apply {
             return text.toString()
         }
-        return ""
     }
 }
